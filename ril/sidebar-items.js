6e767c4cae6ee2a133cb183d09b0271aba39f326
jay3332 <jay3332@users.noreply.github.com>
2022-08-07 21:31:31 +0000

    deploy: 56dfd049bd7457e112d999f2e72c3f1ac7ebf829

--- ril/sidebar-items.js
+++ ril/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["draw",""],["encode",""],["encodings",""],["error",""],["image",""],["pixel",""],["prelude",""],["resize","An interfacing layer between fast_image_resize and this crate."],["sequence",""]]});
+initSidebarItems({"mod":[["draw",""],["encode",""],["encodings",""],["error",""],["image",""],["pixel",""],["prelude",""],["resize","An interfacing layer between `fast_image_resize` and this crate."],["sequence",""]]});
