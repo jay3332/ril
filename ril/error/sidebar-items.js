83e7104309d7244bee2feba611d0d93fe55188cb
jay3332 <jay3332@users.noreply.github.com>
2022-08-11 02:10:41 +0000

    deploy: fdb4b95292ea970b24cbfea506762bdbc8193844

--- ril/error/sidebar-items.js
+++ ril/error/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error","Represents an error that occurs within the crate."]],"type":[["Result",""]]});
+initSidebarItems({"enum":[["Error","Represents an error that occurs within the crate."]],"type":[["Result","A shortcut type equivalent to `Result<T, ril::Error>`."]]});
--- ril/prelude/sidebar-items.js
+++ ril/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ResizeAlgorithm","A filtering algorithm that is used to resize an image."]]});
+initSidebarItems({"enum":[["ImageFormat","Represents the underlying encoding format of an image."],["OverlayMode","The behavior to use when overlaying images on top of each other."],["ResizeAlgorithm","A filtering algorithm that is used to resize an image."]],"struct":[["Image","A high-level image representation."]],"trait":[["Banded","Represents an image with multiple channels, called bands."]]});
