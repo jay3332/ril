ec34b14455ace6fe4cb1297a8857255cdaf5f554
jay3332 <jay3332@users.noreply.github.com>
2022-11-23 03:03:46 +0000

    deploy: a317c67a538f2e1af1203a97bc62046a687f269b

---
+++ ril/quantize/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["quantize_lossy","Quantizes an image using the NeuQuant algorithm."],["quantize_simple","Quantize an image with under 256 colors, panics otherwise. Returns `(palette, image_data)`."]],"struct":[["Quantizer","Configuration options regarding behavior of quantization."]]};
