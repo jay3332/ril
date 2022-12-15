f7933749c6a9bcba5ff97b7bc5b6ad31d47e82b6
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-12-15 13:29:17 -0500

    experimental support for text gradients
    Limitations:
    - much slower than just rendering a gradient masked to text
    - apart from dynamic dispatch, Rust's type system does not allow for different types of fills in text layouts (e.g. `layout.with_basic_text(..., <Rgba>).with_basic_text(..., <LinearGradient>)`  will not work)
    
    Once these limitations are resolved, they can be merged into `main`

--- tests/test_text.rs
+++ tests/test_text.rs
@@ -19,3 +19,26 @@ fn test_text_rendering() -> ril::Result<()> {
     image.draw(&layout);
     image.save_inferred("tests/out/text_render_output.png")
 }
+
+#[test]
+fn test_text_gradient() -> ril::Result<()> {
+    let font = Font::open("tests/test_font_inter.ttf", 50.0)?;
+    let mut image = Image::new(600, 256, Rgba::black());
+
+    let gradient = LinearGradient::new()
+        .with_color(Rgba::new(255, 0, 0, 255))
+        .with_color(Rgba::new(0, 255, 0, 255))
+        .with_color(Rgba::new(0, 0, 255, 255));
+
+    let (cx, cy) = image.center();
+    let layout = TextLayout::new()
+        .with_wrap(WrapStyle::Word)
+        .with_width(image.width())
+        .with_position(cx, cy)
+        .with_basic_text(&font, "this is a ", Rgba::white())
+        .with_basic_text(&font, "gradient", gradient)
+        .centered();
+
+    image.draw(&layout);
+    image.save_inferred("tests/out/text_gradient_output.png")
+}
