90cc6d23435619ca5f2f022e0e643699f2913825
jay3332 <jay3332@users.noreply.github.com>
2023-10-06 22:55:53 +0000

    deploy: bdd1aefb109ee7b2b56f177dba489353e27ae465

--- source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"ril":["",[["encodings",[],["gif.rs","jpeg.rs","mod.rs","png.rs","webp.rs"]]],["draw.rs","encode.rs","error.rs","fill.rs","gradient.rs","image.rs","lib.rs","pixel.rs","quantize.rs","resize.rs","sequence.rs","text.rs"]]\
-}');
-createSourceSidebar();
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"ril":["",[["encodings",[],["gif.rs","jpeg.rs","mod.rs","png.rs","webp.rs"]]],["draw.rs","encode.rs","error.rs","fill.rs","gradient.rs","image.rs","lib.rs","pixel.rs","quantize.rs","resize.rs","sequence.rs","text.rs"]]\
+}');
+createSrcSidebar();
