404cd01d52201d3532994069983a14cc2cc811c1
winston <hey@winston.sh>
2024-04-03 22:26:52 +0200

    test: allow slight delta for lossy YUV/RGB conversion

--- tests/test_webp-pure.rs
+++ tests/test_webp-pure.rs
@@ -1,7 +1,6 @@
 #![cfg(feature = "webp-pure")]
 
 mod common;
-use common::COLORS;
 
 use ril::prelude::*;
 
@@ -23,13 +22,42 @@ fn test_static_webp_decode() -> ril::Result<()> {
 }
 
 #[test]
-fn test_animated_webp_decode() -> ril::Result<()> {
-    for (frame, ref color) in ImageSequence::<Rgb>::open("tests/animated_sample.webp")?.zip(COLORS)
-    {
+fn test_animated_webp_lossless() -> ril::Result<()> {
+    for (i, frame) in ImageSequence::<Rgb>::open("tests/animated_lossless.webp")?.enumerate() {
         let frame = frame?.into_image();
 
-        assert_eq!(frame.dimensions(), (256, 256));
-        assert_eq!(frame.pixel(0, 0), color);
+        let reference =
+            Image::<Rgb>::open(format!("tests/reference/random_lossless-{}.png", i + 1))?;
+
+        frame.map_pixels_with_coords(|x, y, rgb| {
+            assert_eq!(&rgb, reference.get_pixel(x, y).unwrap());
+            rgb
+        });
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_animated_webp_lossy() -> ril::Result<()> {
+    for (i, frame) in ImageSequence::<Rgb>::open("tests/animated_lossy.webp")?.enumerate() {
+        let frame = frame?.into_image();
+
+        let reference = Image::<Rgb>::open(format!("tests/reference/random_lossy-{}.png", i + 1))?;
+
+        let (width, height) = frame.dimensions();
+
+        // https://github.com/image-rs/image-webp/blob/4020925b7002bac88cda9f951eb725f6a7fcd3d8/tests/decode.rs#L56-L59
+        let pixels = frame.pixels();
+        let num_bytes_different = pixels
+            .zip(reference.pixels())
+            .filter(|(a, b)| a != b)
+            .count();
+
+        assert!(
+            100 * num_bytes_different / ((width * height) as usize) < 1,
+            "More than 1% of pixels differ"
+        );
     }
 
     Ok(())
