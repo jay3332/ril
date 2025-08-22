80f66d80a15e91d63942ac73bbd5f5ff5ea13a38
jay3332 <40323796+jay3332@users.noreply.github.com>
2025-08-22 00:10:41 -0400

    add `text_line_height` example output

--- examples/text_line_height.rs
+++ examples/text_line_height.rs
@@ -1,20 +1,20 @@
 use ril::prelude::*;
 
 fn main() -> ril::Result<()> {
-    let font = Font::open("./examples/assets/Arial.ttf", 12.0)?;
+    let font = Font::open("./examples/assets/Arial.ttf", 24.0)?;
 
-    let mut image = Image::new(72, 72, Rgba::black());
+    let mut image = Image::new(200, 200, Rgba::black());
 
     let (x, y) = image.center();
     let layout = TextLayout::new()
         .centered() // Shorthand for centering horizontally and vertically
-        .with_wrap(WrapStyle::Word, image.width()) // Wrap the text such that it doesn't overflow the image
+        .with_wrap(image.width(), WrapStyle::Word) // Wrap the text such that it doesn't overflow the image
         .with_position(x, y) // Position the anchor (which is the center) at the center of the image
         .with_line_height(0.8) // Set the line height to 80% of the default
         .with_basic_text(&font, "Super long overflowing line of text", Rgba::white());
 
     image.draw(&layout);
-    image.save_inferred("example_text_line_height.png")?;
+    image.save_inferred("examples/out/text_line_height.png")?;
 
     Ok(())
 }
